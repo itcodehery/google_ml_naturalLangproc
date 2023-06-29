062e52eb6f0160b882426eceea9982809cfa5278
Hari Prasad <haririo321@gmail.com>
2023-06-29 16:48:23 +0530

    Got the InputImage

--- naturallangproc/lib/barcodescanner.dart
+++ naturallangproc/lib/barcodescanner.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:google_mlkit_barcode_scanning/google_mlkit_barcode_scanning.dart';
 
 class BarCodeScanner extends StatelessWidget {
   const BarCodeScanner({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final InputImage inputImage;
     return Scaffold(
       appBar: AppBar(
         title: const Text("Bar Code Reader"),

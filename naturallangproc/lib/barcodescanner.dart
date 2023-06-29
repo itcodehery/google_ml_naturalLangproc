5c76505ae2f2bd5ce012f5ebcfcc17f28f821568
Hari Prasad <haririo321@gmail.com>
2023-06-29 17:02:45 +0530

    Made it worse, but there is a way

--- naturallangproc/lib/barcodescanner.dart
+++ naturallangproc/lib/barcodescanner.dart
@@ -1,16 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:google_mlkit_barcode_scanning/google_mlkit_barcode_scanning.dart';
 
-class BarCodeScanner extends StatelessWidget {
-  const BarCodeScanner({Key? key}) : super(key: key);
+class BarcodeScanPage extends StatefulWidget {
+  const BarcodeScanPage({Key? key}) : super(key: key);
 
   @override
+  BarcodeScanPageState createState() => BarcodeScanPageState();
+}
+
+class BarcodeScanPageState extends State<BarcodeScanPage> {
+  final BarcodeScanner _barcodeScanner = BarcodeScanner();
+  bool _canProcess = true;
+  bool _isBusy = false;
+  CustomPaint? _customPaint;
+  String? _text;
+
+  @override
+  void dispose() {
+    _canProcess = false;
+    _barcodeScanner.close();
+    super.dispose();
+  }
+
+   @override
   Widget build(BuildContext context) {
-    final InputImage inputImage;
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Bar Code Reader"),
-      ),
+    return CameraView(
+      title: 'Barcode Scanner',
+      customPaint: _customPaint,
+      text: _text,
+      onImage: (inputImage) {
+        processImage(inputImage);
+      },
     );
   }
-}
